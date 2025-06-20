1a34a379cda56dc57364f6eb18562b7e2887fd08
thneri95 <tiago.neri1995@outlook.com>
2025-06-20 20:40:25 -0300

    up

--- models/Pessoas.cs
+++ models/Pessoas.cs
@@ -5,10 +5,10 @@ public class Person
     public string Name { get; set; }
     public int Age { get; set; }
 
-    public string NameRepresentativePF { get; set; } // Changed to English: NameRepresentativePF
+    public string NameRepresentativePF { get; set; }
 
-    public void Present() // Removed semicolon and corrected the method body
+    public void Present()
     {
-        Console.WriteLine($"Hello, my name is {Name} and I'm {Age} years old."); // Corrected interpolation and added "years old"
+        Console.WriteLine($"Hello, my name is {Name} and I'm {Age} years old.");
     }
 }
