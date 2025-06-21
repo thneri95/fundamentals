19270db1f3a969a28f835e786bb1ef75611ea183
thneri95 <tiago.neri1995@outlook.com>
2025-06-20 21:29:44 -0300

    update

--- Program.cs
+++ Program.cs
@@ -1,5 +1,84 @@
 ﻿
-using Fundamentals.Models;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// string option;
+// bool showMenu = true;
+
+// while (showMenu)
+// {
+//     Console.Clear(); // Clears the console screen
+
+//     Console.WriteLine("Enter your option:");
+//     Console.WriteLine("1 - Register Client");
+//     Console.WriteLine("2 - Search Client");
+//     Console.WriteLine("3 - Delete Client");
+//     Console.WriteLine("4 - Exit");
+
+//     option = Console.ReadLine();
+
+//     switch (option)
+//     {
+//         case "1":
+//             Console.WriteLine("Client Registration");
+//             Console.WriteLine("Press any key to continue...");
+//             Console.ReadKey();
+//             break;
+
+//         case "2":
+//             Console.WriteLine("Client Search");
+//             Console.WriteLine("Press any key to continue...");
+//             Console.ReadKey();
+//             break;
+
+//         case "3":
+//             Console.WriteLine("Delete Client");
+//             Console.WriteLine("Press any key to continue...");
+//             Console.ReadKey();
+//             break;
+
+//         case "4":
+//             Console.WriteLine("Exiting...");
+//             showMenu = false; // Set to false to exit the loop
+//             break;
+
+//         default:
+//             Console.WriteLine("Invalid option. Please try again.");
+//             Console.WriteLine("Press any key to continue...");
+//             Console.ReadKey();
+//             break;
+//     }
+
+//     Console.WriteLine("The program is closed");
+
+// }
+
 
 
 
